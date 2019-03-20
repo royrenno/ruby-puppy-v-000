6aeb4dc73a6032249cdb49d6582db9aeb53161da
roy renno <royrenno@yahoo.com>
2019-03-20 06:36:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,11 @@ class Dog
       @name = name
       @@all << self
     end
-  end
+
+    def self.all
+      puts @@all.map{ |dog| dog.name}
+
+    end
 
       def self.clear_all
      @@all.clear_all
